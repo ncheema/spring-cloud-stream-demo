c301b2d744684ceea3d96d57dda4689fe0c81489
ncheema <navjot_cheema@intuit.com>
2020-01-05 10:42:13 -0800

    switch kotlin to java

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private EmitterProcessor<Message<String>> processor =  EmitterProcessor.create();
+
+    @Bean
+    public Supplier<Flux<Message<String>>> supplier() {
+        return () -> processor;
+    }
+
+    @Bean
+    public Consumer<String> consumer() {
+        return msg -> processor.onNext(MessageBuilder.withPayload(msg).build());
+    }
+}
